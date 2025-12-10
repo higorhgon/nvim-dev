a18cc58d9feb10c8409aadcfba51cbec40a2d83f
Higor Henrique Prata Gonçalves <higor.henrique@legisweb.com.br>
2025-12-10 16:09:17 -0300

    teste

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
 require("config.options")
 require("config.pack")
 require("config.colorscheme")
-require("config.plugins.mini")
-require("config.plugins.treesitter")
-require("config.plugins.lsp-stuff")
-
+require("config.plugins")
 require("config.keymaps")
 require("config.autocmds")
