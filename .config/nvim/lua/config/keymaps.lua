c529e08f18f0add4e5f6cd15f98daa84240000a1
Higor Henrique Prata Gonçalves <higor.henrique@legisweb.com.br>
2025-12-10 16:18:00 -0300

    git picker

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,6 +9,7 @@ vim.keymap.set(
 )
 vim.keymap.set("n", "<Leader>E", ":lua MiniFiles.open()<CR>", { desc = "Explore CWD" })
 vim.keymap.set("n", "<Leader>ff", ":Pick files<CR>", { desc = "Find Files" })
+vim.keymap.set("n", "<Leader>fg", ":Pick files tool='git'<CR>", { desc = "Find Files GIT" })
 vim.keymap.set("n", "<Leader>fh", ":Pick help<CR>", { desc = "Find Help" })
 vim.keymap.set("n", "<Leader>fb", ":Pick buffers<CR>", { desc = "Find Buffers" })
 vim.keymap.set("n", "<Leader>sg", ":Pick grep<CR>", { desc = "Search Grep" })
