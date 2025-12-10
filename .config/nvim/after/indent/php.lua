0a957a58bde92c6959412fcaa9d436248a56117d
Higor Henrique Prata Gonçalves <higor.henrique@legisweb.com.br>
2025-12-10 15:33:47 -0300

    simplificado a configuracao

--- .config/nvim/after/indent/php.lua
+++
@@ -1,5 +0,0 @@
-vim.o.autoindent = true
-vim.o.smartindent = true
--- vim.o.indentexpr = ""
-vim.g.php_indent_closing = 0
-vim.g.php_indent_auto = 1
--- .config/nvim/lua/plugins/user.lua
+++
@@ -1,9 +0,0 @@
--- Este arquivo importa configurações extras do usuário
-return {
-    -- Importando a config do colorscheme
-    require("config.colorscheme")[1],
-    require("config.keymaps").setup(),
-
-    -- Você pode adicionar outros plugins diretamente aqui também
-    -- { "plugin/exemplo" },
-}
