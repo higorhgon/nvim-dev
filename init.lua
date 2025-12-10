1359394857f2bb6fc05e949ccdd03bba2fe319dd
Higor Henrique Prata Gonçalves <higor.henrique@legisweb.com.br>
2025-12-10 16:14:23 -0300

    teste 3

--- init.lua
+++ init.lua
@@ -1,6 +1,14 @@
 require("config.options")
 require("config.pack")
 require("config.colorscheme")
-require("config.plugins")
+
+-- Dynamically load all plugin configurations from the "plugins" directory
+local plugins_dir = vim.fn.stdpath("config") .. "/lua/plugins"
+
+for _, file in ipairs(vim.fn.readdir(plugins_dir, [[v:val =~ '\.lua$']])) do
+    local plugin_path = "plugins." .. file:gsub("%.lua$", "")
+    pcall(require, plugin_path)
+end
+
 require("config.keymaps")
 require("config.autocmds")
