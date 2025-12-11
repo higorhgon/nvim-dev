7e7c7946def732cc413d29f5a215a037053a5ce0
Higor Henrique Prata Gonçalves <higor.henrique@legisweb.com.br>
2025-12-11 11:38:44 -0300

    correcao de sintaxe

--- init.lua
+++ init.lua
@@ -4,10 +4,13 @@ require("config.colorscheme")
 
 -- Dynamically load all plugin configurations from the "plugins" directory
 local plugins_dir = vim.fn.stdpath("config") .. "/lua/plugins"
+local all_files = vim.fn.readdir(plugins_dir)
 
-for _, file in ipairs(vim.fn.readdir(plugins_dir, [[v:val =~ '\.lua$']])) do
-    local plugin_path = "plugins." .. file:gsub("%.lua$", "")
-    pcall(require, plugin_path)
+for _, file in ipairs(all_files) do
+    if file:match("%.lua$") then
+        local plugin_name = file:gsub("%.lua$", "")
+        pcall(require, "plugins." .. plugin_name)
+    end
 end
 
 require("config.keymaps")
